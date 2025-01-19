e27bdaaea11afcefe6db585c13a824e6dee71536
Andrii Kliuiko <32270840+andrUIko@users.noreply.github.com>
2025-01-19 21:08:17 +0100

    refactor of merge sorted array

--- src/merge_sorted_array.rs
+++ src/merge_sorted_array.rs
@@ -3,11 +3,9 @@ pub struct Solution;
 
 impl Solution {
     pub fn merge(nums1: &mut Vec<i32>, m: i32, nums2: &mut Vec<i32>, n: i32) {
-        let (mut i, mut j, mut k) = (
-            usize::try_from(m).unwrap(),
-            usize::try_from(n).unwrap(),
-            usize::try_from(m + n).unwrap() - 1,
-        );
+        let mut i = usize::try_from(m).unwrap();
+        let mut j = usize::try_from(n).unwrap();
+        let mut k = usize::try_from(m + n - 1).unwrap();
 
         while i > 0 && j > 0 {
             if nums1[i - 1] > nums2[j - 1] {
